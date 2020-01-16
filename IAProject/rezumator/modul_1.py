2aee153510c4dfee62b730d3e255ab023b4e11bb
platicaalex <platicaalex@gmail.com>
2020-01-16 16:50:18 +0200

    Proiect

--- IAProject/rezumator/modul_1.py
+++ IAProject/rezumator/modul_1.py
@@ -1,108 +1,115 @@
 import string
-import re
-import os
+
+import nltk
+
+nltk.download('punkt')
 
 
-#Functie care deschide fisierul si adauga toate prepozitiile / conjuctiile intr-o lista
+# Functie care deschide fisierul si adauga toate prepozitiile / conjuctiile intr-o lista
 def adauga_cuvinte(path):
-    with open(path, "r" , encoding='utf8') as f:
+    with open(path, "r", encoding='utf8') as f:
         cuvinte = f.read()
         lista_cuvinte = cuvinte.split("\n")
     return lista_cuvinte[:-1]
 
-#Functie care returneaza un dictionar avand ca cheie cuvintele si ca valoare numarul.
-#De aparitii in text a acestora
+
+# Functie care returneaza un dictionar avand ca cheie cuvintele si ca valoare numarul.
+# De aparitii in text a acestora
 
 def word_counter(text):
-    dictionar_couter={}
-    words=[word.strip(string.punctuation).lower() for word in text.split()]
+    dictionar_couter = {}
+    words = [word.strip(string.punctuation).lower() for word in text.split()]
     for word in words:
         if word not in dictionar_couter:
-            dictionar_couter[word]=1
+            dictionar_couter[word] = 1
         else:
-            dictionar_couter[word]=dictionar_couter[word]+1
+            dictionar_couter[word] = dictionar_couter[word] + 1
 
     if '' in dictionar_couter:
         del dictionar_couter['']
 
     return dictionar_couter
 
-#Functie care imparte in propozitii un text.
-def text_sentences(text):
-    sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
-    return sentences[:-1]
 
-#Functie eliminare dialog
+# Functie care imparte in propozitii un text.
+def text_sentences(text):
+    sentences = nltk.tokenize.sent_tokenize(text)
+    return sentences
+
+
+# Functie eliminare dialog
 def process_text(text):
     split_text = text.split("\n")
     new_text = ""
-    for counter in range(0,len(split_text)):
-        if split_text[counter][-2] == ':' and split_text[counter+1][0] == '-':
+    for counter in range(0, len(split_text)):
+        if split_text[counter][-2] == ':' and split_text[counter + 1][0] == '-':
             pass
         elif split_text[counter][0] == '-':
             pass
         else:
-            new_text = new_text+split_text[counter] + "\n"
+            new_text = new_text + split_text[counter] + "\n"
 
     return new_text
 
-#Functie care imparte un text in paragrafe.
+
+# Functie care imparte un text in paragrafe.
 def paragraphs_text(text):
     new_text = text.split("\n")
     if new_text[-1] == '':
-        return  new_text[:-1]
+        return new_text[:-1]
     else:
         return new_text
 
-#Functie pentru distributia frecventei cuvintelor.
-#Functia elimina din dictionarul frecventei cuvintele de legatura, prepozitii, conjunctii
+
+# Functie pentru distributia frecventei cuvintelor.
+# Functia elimina din dictionarul frecventei cuvintele de legatura, prepozitii, conjunctii
 def word_freq_distribution(dictionar_counter):
     freq_dictionar = {}
-    cuv_neimportante = adauga_cuvinte(r"C:\Users\Robert\Desktop\IA Project\Cuv-neimportante.txt");
+    cuv_neimportante = adauga_cuvinte(r"C:\Users\FuNNNk\Desktop\Fac\Sem5\AI\RezumatorOk\Cuv-neimportante.txt")
     max_freq = max(dictionar_counter.values())
     for word in dictionar_counter.keys():
         if len(word) == 2 or len(word) == 1 or word in cuv_neimportante:
             pass
         else:
-            freq_dictionar[word] = (dictionar_counter[word]/max_freq)
-    list_freq = sorted(freq_dictionar.items(), key=lambda x:x[1] ,reverse=True)
+            freq_dictionar[word] = (dictionar_counter[word] / max_freq)
+    list_freq = sorted(freq_dictionar.items(), key=lambda x: x[1], reverse=True)
     return list_freq
 
-#Functie prin care determina cele mai importante cuvinte din text
+
+# Functie prin care determina cele mai importante cuvinte din text
 def extrage_cuvinte_imp(list_freq):
     cuvinte_importante = []
-    for counter in range(0,len(list_freq)):
-        if counter <= len(list_freq)/10: #Luam primele 10% cuvintele aparute cel mai frecvent in list_freq
+    for counter in range(0, len(list_freq)):
+        if counter <= len(list_freq) / 10:  # Luam primele 10% cuvintele aparute cel mai frecvent in list_freq
             cuvinte_importante.append(list_freq[counter][0])
     return cuvinte_importante
 
 
-
 ####################################
-#Transformare numere litere in cifre
+# Transformare numere litere in cifre
 ####################################
 def litere_in_cifre(textnum, numwords={}):
     textnum = procesare_numere(textnum)
     if not numwords:
-      units = [
-        "zero", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt",
-        "noua", "zece", "unsprezece", "doisprezece", "treisprezece", "paisprezece", "cincisprezece",
-        "saisprezece", "saptesprezece", "optisprezece", "nouasprezece",
-      ]
+        units = [
+            "zero", "unu", "doi", "trei", "patru", "cinci", "sase", "sapte", "opt",
+            "noua", "zece", "unsprezece", "doisprezece", "treisprezece", "paisprezece", "cincisprezece",
+            "saisprezece", "saptesprezece", "optisprezece", "nouasprezece",
+        ]
 
-      tens = ["", "", "douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci"]
+        tens = ["", "", "douazeci", "treizeci", "patruzeci", "cincizeci", "saizeci", "saptezeci", "optzeci", "nouazeci"]
 
-      scales = ["suta", "mie", "milion"]
+        scales = ["suta", "mie", "milion"]
 
-      numwords["si"] = (1, 0)
-      for idx, word in enumerate(units):    numwords[word] = (1, idx)
-      for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
-      for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+        numwords["si"] = (1, 0)
+        for idx, word in enumerate(units):    numwords[word] = (1, idx)
+        for idx, word in enumerate(tens):     numwords[word] = (1, idx * 10)
+        for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
 
     current = result = 0
     for word in textnum.split():
         if word not in numwords:
-          raise Exception("Numar incorect: " + word)
+            raise Exception("Numar incorect: " + word)
 
         scale, increment = numwords[word]
         current = current * scale + increment
@@ -112,22 +119,53 @@ def litere_in_cifre(textnum, numwords={}):
 
     return result + current
 
+
 def procesare_numere(text):
-    numere=text.split(" ")
-    numar_nou=''
-    for counter in range(0,len(numere)):
-        if numere[counter] == 'o' or numere[counter] == 'un' :
-            numar_nou=numar_nou + 'unu' + ' '
-        elif numere[counter] == 'doua' and numere[counter+1] != '':
-            numar_nou=numar_nou +'doi' + ' '
+    numere = text.split(" ")
+    numar_nou = ''
+    for counter in range(0, len(numere)):
+        if numere[counter] == 'o' or numere[counter] == 'un':
+            numar_nou = numar_nou + 'unu' + ' '
+        elif numere[counter] == 'doua' and numere[counter + 1] != '':
+            numar_nou = numar_nou + 'doi' + ' '
         elif numere[counter] == 'sute':
-            numar_nou=numar_nou + 'suta' + ' '
+            numar_nou = numar_nou + 'suta' + ' '
         elif numere[counter] == 'mii':
-            numar_nou=numar_nou + 'mie' + ' '
+            numar_nou = numar_nou + 'mie' + ' '
         elif numere[counter] == 'milioane':
-            numar_nou=numar_nou + 'milion' + ' '
+            numar_nou = numar_nou + 'milion' + ' '
         elif numere[counter] == 'de':
             pass
-        else :
-            numar_nou=numar_nou + numere[counter] + ' '
+        else:
+            numar_nou = numar_nou + numere[counter] + ' '
     return numar_nou
+
+
+# def remove_enum(text):
+#     text = re.sub("(\w+)( \w+)*, (\w+)( \w+)*(, (\w+)( \w+)*)*", "", text)
+#     return text
+#
+#
+# def remove_rep(text):
+#     new_text = ""
+#     sentences = nltk.tokenize.sent_tokenize(text)
+#     print(sentences)
+#     for sentence in sentences:
+#         words = re.findall(r'[a-zA-Z-*]+', sentence)
+#         words_dict = list(dict.fromkeys(words))
+#         print(words_dict)
+#         for word in words_dict:
+#             print(word)
+#             print(words.count(word))
+#             if words.count(word) > 1:
+#                 sentence = sentence.replace(word, "", words.count(word) - 1)
+#                 print(sentence)
+#         new_text += sentence
+#         new_text = re.sub(r'si', '', new_text)
+#         new_text = re.sub(r' +', ' ', new_text)
+#
+#     return new_text
+#
+# print(remove_rep("el s-a uitat si s-a uitat si s-a uitat"))
+# print(remove_rep("el manca si zi si noapte si zi si noapte"))
+
--- IAProject/rezumator/modul_2.py
+++ IAProject/rezumator/modul_2.py
@@ -1,9 +1,10 @@
-from . import modul_1 # Modulul (Robert Darabana - Grigorovschi Teodor)
+from . import modul_1  # Modulul (Robert Darabana - Grigorovschi Teodor)
 import string
 
-#Afisarea textului rezumat in functie de procentul dat de utilizator
-def afisare_text_procent(lista_scor_prop,procent):
-    lista_scor_prop = lista_scor_prop[0:round(len(lista_scor_prop)*(100-procent)/100)]
+
+# Afisarea textului rezumat in functie de procentul dat de utilizator
+def afisare_text_procent(lista_scor_prop, procent):
+    lista_scor_prop = lista_scor_prop[0:round(len(lista_scor_prop) * procent / 100)]
     return lista_scor_prop
 
 
@@ -11,9 +12,9 @@ def afisare_text_procent(lista_scor_prop,procent):
 ############################EURISTICI################################
 #####################################################################
 
-def calculeaza_scor(propozitie,text):
+def calculeaza_scor(propozitie, text):
     list_freq = modul_1.word_freq_distribution(modul_1.word_counter(text))
-    cuvinte_importante = modul_1.extrage_cuvinte_imp(list_freq) #10% cele mai importante cuvinte din text
+    cuvinte_importante = modul_1.extrage_cuvinte_imp(list_freq)  # 10% cele mai importante cuvinte din text
     words = [word.strip(string.punctuation).lower() for word in propozitie.split()]
     begin, end, total_words, cuv_imp = 0, 0, 0, 0
     for word in words:
@@ -24,10 +25,10 @@ def calculeaza_scor(propozitie,text):
             end = total_words
         total_words = total_words + 1
 
-    if end-begin+1 == 0:
+    if end - begin + 1 == 0:
         return 0.0
 
-    return  float(cuv_imp**2) / float(end-begin+1)
+    return float(cuv_imp ** 2) / float(end - begin + 1)
 
 
 def scorul_propozitiilor(text):
@@ -36,27 +37,35 @@ def scorul_propozitiilor(text):
     propozitii_text = modul_1.text_sentences(text)
     for prop in propozitii_text:
         if prop not in dictionar_scor:
-            dictionar_scor[prop] = calculeaza_scor(prop,text)
+            dictionar_scor[prop] = calculeaza_scor(prop, text)
         else:
             pass
 
-    lista_scor_prop = sorted(dictionar_scor.items(), key=lambda x:x[1] ,reverse=True)
+    lista_scor_prop = sorted(dictionar_scor.items(), key=lambda x: x[1], reverse=True)
     return lista_scor_prop
 
-def reconstruire_text(text,procent):
+
+def reconstruire_text(text, procent):
     text_reconstruit = ""
     lista_scor_prop = scorul_propozitiilor(text)
-    prop_alese = afisare_text_procent(lista_scor_prop, int(procent)) # Prop-alese = lista de tuple de forma ( propozitie , procent)
-    lista_prop_alese =[] #Doar propozitiile din tupla de forma (propozitie, procent)
+    prop_alese = afisare_text_procent(lista_scor_prop,
+                                      int(procent))  # Prop-alese = lista de tuple de forma ( propozitie , procent)
+    lista_prop_alese = []  # Doar propozitiile din tupla de forma (propozitie, procent)
     for prop in prop_alese:
-        lista_prop_alese.append(prop[0])
+        s = prop[0].replace('\r', "").replace('\n', "")
+        lista_prop_alese.append(s)
     text = modul_1.process_text(text)
     paragrafe_text = modul_1.paragraphs_text(text)
     for paragraf in paragrafe_text:
+        ok = 0
         prop_paragraf = modul_1.text_sentences(paragraf)
         for prop in prop_paragraf:
             if prop in lista_prop_alese:
-                text_reconstruit = text_reconstruit + prop + ". "
-        text_reconstruit = text_reconstruit + "\n"
+                text_reconstruit = text_reconstruit + prop + ' '
+                ok = 1
+            else:
+                print(prop)
+        if ok == 1:
+            text_reconstruit =text_reconstruit + "\n" + "   "
 
     return text_reconstruit
